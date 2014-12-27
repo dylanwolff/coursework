2c0fb2cdc944355aed44e51460618d23863f3e33
Dylan Wolff <github@dw.id.au>
2014-12-27 19:34:00 +1100

    Committing exercises

--- tealeaf-intro-to-programming/flow_control/ex3.rb
+++ tealeaf-intro-to-programming/flow_control/ex3.rb
@@ -6,6 +6,6 @@ number = gets.chomp.to_i
   elsif number <= 50
     puts "The number #{number} is between 0 and 50"
   elsif number <= 100
-    puts "The number #{number} is between 50 and 100"
+    puts "The number #{number} is between 51 and 100"
   else puts "The number #{number} is greater than 100"
   end
