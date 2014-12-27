ea9656ad89d68fe21456f3cfdfcf3c5b9716df8e
Dylan Wolff <github@dw.id.au>
2014-12-27 16:38:38 +1100

    Committing exercises

---
+++ tealeaf-intro-to-programming/variables/name.rb
@@ -0,0 +1,9 @@
+puts "Hi there! What's your first name?"
+first = gets.chomp
+puts "What's your second name"
+second = gets.chomp
+puts "Hi " + first + " " + second
+
+10.times do
+  puts first + " " + second
+end
