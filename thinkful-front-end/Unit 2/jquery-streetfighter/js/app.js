0dee5fafe1dc4ace688c2ffe8575367c382e4080
Dylan Wolff <dylanwolff@gmail.com>
2015-01-07 19:02:48 +1100

    Complete up to & including assignment 4

---
+++ thinkful-front-end/Unit 2/jquery-streetfighter/js/app.js
@@ -0,0 +1,56 @@
+// Page loads, Ryu standing still
+// Hover over Ryu, shifts to ready pose
+// Stop hover, goes back to still
+// Click and hold, throws a Hadouken
+// Release click, returns to ready pose
+// Hold "x", assumes "looking cool" pose
+// Release "x", returns to ready pose
+
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    //play hadouken sound
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    //show hadouken
+    $('.hadouken').show()
+    $('.hadouken').finish().show()
+    //animate hadouken to the right
+    .animate(
+    {'left': '300px'},
+    500,
+    function() {
+      $(this).hide();
+      $(this).css('left', '-200px');
+    }
+  );
+})
+  .mouseup(function() {
+    //ryu goes back to ready position
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+
+
+
+
+
+
+
+
