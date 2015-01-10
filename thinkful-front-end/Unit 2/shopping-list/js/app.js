23df6b649c377f9b6444e259306a37bd42aa5bc5
Dylan Wolff <dylanwolff@gmail.com>
2015-01-11 10:19:40 +1100

    JS rough in

---
+++ thinkful-front-end/Unit 2/shopping-list/js/app.js
@@ -0,0 +1,14 @@
+//Add item to list
+$('.add-button').click(function() {
+    $('.list').append('<li class="todo"><input type="checkbox"><span>' 
+      + $('#add-item').val() 
+      + '</span><button class="delete-button">Delete</button></li>');
+    $('#add-item').val("");
+  }
+)
+
+//Toggle item as completed/incomplete
+
+
+
+//Delete item from list
