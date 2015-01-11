ffc0436311eef91764882583641e570c5e22d479
Dylan Wolff <dylanwolff@gmail.com>
2015-01-11 19:20:04 +1100

    Initial commit

---
+++ thinkful-front-end/unit-3/fizzbuzz/js/app.js
@@ -0,0 +1,15 @@
+//Count from 1 to 100
+  //if divisible by 3 and 5, print "fizzbuzz"
+    //if number % 3 = 0 && number % 3 = 0
+    //print fizzbuzz
+  //If divisible by 3, print "fizz"
+    //elseif number % 3 = 0
+    //print fizz
+  //If divisible by 5, print "buzz"
+    //elseif number % 5 = 0
+    //print buzz
+  //If not divisible by 3 or 5, print number
+    //else
+    //print number
+
+  //Modulus - ie 36 % 3 = 0
