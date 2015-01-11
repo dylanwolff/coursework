81fd3c4cd3d9be1a2f1938819df88cba96ade551
Dylan Wolff <dylanwolff@gmail.com>
2015-01-11 12:02:36 +1100

    Add complete & delete functions

--- thinkful-front-end/Unit 2/shopping-list/js/app.js
+++ thinkful-front-end/Unit 2/shopping-list/js/app.js
@@ -1,14 +1,27 @@
-//Add item to list
-$('#add-button').click(function() {
-    $('.list').append('<li class="todo"><input type="checkbox"><span>' 
-      + $('#add-item').val() 
+$(document).ready(function() { 
+  
+  //Add item to list
+    //Need to add validation so empty items can't be added
+  $("#add-button").click(function() {
+    $("ul").append('<li class="todo"><input type="checkbox"><span>' 
+      + $("#add-item").val() 
       + '</span><button class="delete-button">Delete</button></li>');
-    $('#add-item').val("");
-  }
-)
+    $("#add-item").val("");
+  });
 
-//Toggle item as completed/incomplete
+  //Toggle item as completed/incomplete
+    //Need to add event listener for li that don't exist when page loads
+  $("input[type='checkbox']").change(function() {
+   $(this).closest("li").toggleClass("todo done");
+  });
+
+
+  //Delete item from list
+    //Need to add event listener for li that don't exist when page loads
+  $(".delete-button").click(function() { 
+    $(this).parent().remove();
+  });
 
 
 
-//Delete item from list
+});
