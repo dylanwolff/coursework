714f6ad017c8fbc9c12659388894cceb91f4aa92
Dylan Wolff <dylanwolff@gmail.com>
2015-01-12 17:11:07 +1100

    Tidy comments

--- thinkful-front-end/unit-3/fizzbuzz/js/app.js
+++ thinkful-front-end/unit-3/fizzbuzz/js/app.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   //Count from 1 to 100
   for (i = 1; i <101; i++) {
-    //if evenly divisible by 3 and 5, print "fizzbuzz"
+    //if number is evenly divisible by 3 and 5, append "FizzBuzz" to #list
     if (i % 3 === 0 && i % 5 === 0) {
       $("#list").append("<li>FizzBuzz</li>");
-      //If evenly divisible by 3, print "fizz"
+      //If number is evenly divisible by 3, append "Fizz" to #list
     } else if (i % 3 === 0) {
       $("#list").append("<li>Fizz</li>");
-      //If evenly divisible by 5, print "buzz"
+      //If number is evenly divisible by 5, append "Buzz" to #list
     } else if (i % 5 === 0) {
       $("#list").append("<li>Buzz</li>");
-      //If not evenly divisible by 3 or 5, print number
+      //If number is not evenly divisible by 3 or 5, append the number to #list
     } else {
       $("#list").append("<li>" + i + "</li>");
     }
