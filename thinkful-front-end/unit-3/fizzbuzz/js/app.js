cc7b44ccc8d53ba9eac98f4e587a78d2bea3fa24
Dylan Wolff <dylanwolff@gmail.com>
2015-01-12 13:01:28 +1100

    Project completed

--- thinkful-front-end/unit-3/fizzbuzz/js/app.js
+++ thinkful-front-end/unit-3/fizzbuzz/js/app.js
@@ -1,37 +1,21 @@
-//Count from 1 to 100
-  //if divisible by 3 and 5, print "fizzbuzz"
-    //if number % 3 = 0 && number % 3 = 0
-    //print fizzbuzz
-  //If divisible by 3, print "fizz"
-    //else if number % 3 = 0
-    //print fizz
-  //If divisible by 5, print "buzz"
-    //else if number % 5 = 0
-    //print buzz
-  //If not divisible by 3 or 5, print number
-    //else
-    //print number
-
-  //Modulus - ie 36 % 3 = 0
-
-for (i = 1; i <= 100; i++)
-{
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
+$(document).ready(function() {
+  //Count from 1 to 100
+  for (i = 1; i <101; i++) {
+    //if evenly divisible by 3 and 5, print "fizzbuzz"
+    if (i % 3 === 0 && i % 5 === 0) {
+      $("#list").append("<li>FizzBuzz</li>");
+      //If evenly divisible by 3, print "fizz"
+    } else if (i % 3 === 0) {
+      $("#list").append("<li>Fizz</li>");
+      //If evenly divisible by 5, print "buzz"
+    } else if (i % 5 === 0) {
+      $("#list").append("<li>Buzz</li>");
+      //If not evenly divisible by 3 or 5, print number
+    } else {
+      $("#list").append("<li>" + i + "</li>");
+    }
   }
-  else if (i % 3 == 0) {
-    console.log("Fizz");
-  }
-  else if (i % 5 == 0) {
-    console.log("Buzz");
-  }
-  else {
-    console.log(i);
-  }
-}
-
-
-
+})
 
 
 
