a37a48c850a3435cafffcfa1e41664102869f9c7
Dylan Wolff <dylanwolff@gmail.com>
2015-01-14 18:08:48 +1100

    Add newGame function

--- thinkful-front-end/unit-3/hot-or-cold/js/app.js
+++ thinkful-front-end/unit-3/hot-or-cold/js/app.js
@@ -1,20 +1,7 @@
 
 $(document).ready(function(){
 	
-  //Page loads, starts a new game
-  //Computer randomly selects number between 1-100
-    //Code to ensure input is numeric and between 1-100
-    //Player guesses the number
-      //Feedback "very Hot" in #feedback if the guess is 1-10 away
-      //Feedback "Hot" in #feedback if the guess is close 11-20 away
-      //Feedback "Warm" in #feedback if the guess is 21-30 away
-      //Feedback "Cold" in #feedback if the guess is 31-50 away
-      //Feedback "Ice Cold" in #feedback if the guess is 51+ away
-      //Feedback in #feedback "Good guess" or similar if the guess is correct
-        //Option to start a new game
-      //Track number of guesses in #count
-      //Add numbers guessed so far to #guesslist
-    //Can start a new game by clicking "New Game" button
+  //Need to indent code correctly
 
   //VARIABLES
   var randomNumber = 0;
@@ -31,7 +18,6 @@ $(document).ready(function(){
   getRandomNumber();
 
   //Get guess from user, increment #count and add to #guesslist
-    //Need to add validation (only an integer from 1-100)
   $("form").submit(function(event){
     event.preventDefault();
       guess = parseInt($("#userGuess").val(), 10);
@@ -39,14 +25,28 @@ $(document).ready(function(){
       guessCount++;
       setCount(guessCount);
       $("ul#guessList").append("<li>" + guess + "</li>");
+      $("#userGuess").val("");
   });
+
+  /*
+  //Validate user input
+  function validate(){
+    if(isNan(guess)){
+      alert("Enter a number from 1 to 100");
+    } else if(guess === ""){
+      alert("Enter a number from 1 to 100"); 
+    } else if(guess < 0 || guess > 100){
+      alert("Enter a number from 1 to 100")
+    }
+  }
+  */
 
   //Gets whether the difference between randomNumber and guess is +ve or -ve
   function compareDiff(){
     if (guess - randomNumber > 0) {
-        negativeDiff();
-        console.log("The random number is lower than the guess");
-    }  else {
+      negativeDiff();
+      console.log("The random number is lower than the guess");
+    } else {
         positiveDiff();
         console.log("The random number is higher than the guess");
     }
@@ -56,16 +56,16 @@ $(document).ready(function(){
   function positiveDiff(){
     if (guess / randomNumber === 1) {
       setFeedback("You're right! Good guess!");
-  }  else if ((randomNumber - guess) > 50.5){
-        setFeedback("You're ice cold!");
+  } else if ((randomNumber - guess) > 50.5){
+      setFeedback("You're ice cold!");
   }  else if ((randomNumber - guess) > 30.5){
-        setFeedback("You're cold!");
+      setFeedback("You're cold!");
   }  else if ((randomNumber - guess) > 20.5){
-        setFeedback("You're warm!");
+      setFeedback("You're warm!");
   }  else if ((randomNumber - guess) > 10.5){
-        setFeedback("You're hot!");
+      setFeedback("You're hot!");
   }  else { 
-        setFeedback("You're very hot!");
+      setFeedback("You're very hot!");
      } 
   }
 
@@ -74,15 +74,15 @@ $(document).ready(function(){
     if (guess / randomNumber === 1) {
       setFeedback("You're right! Good guess!");
   }  else if ((guess - randomNumber) > 50.5){
-        setFeedback("You're ice cold!");
+      setFeedback("You're ice cold!");
   }  else if ((guess - randomNumber) > 30.5){
-        setFeedback("You're cold!");
+      setFeedback("You're cold!");
   }  else if ((guess - randomNumber) > 20.5){
-        setFeedback("You're warm!");
+      setFeedback("You're warm!");
   }  else if ((guess - randomNumber) > 10.5){
-        setFeedback("You're hot!");
+      setFeedback("You're hot!");
   }  else { 
-        setFeedback("You're very hot!");
+      setFeedback("You're very hot!");
      } 
   }
 
@@ -96,6 +96,20 @@ $(document).ready(function(){
     $("#count").text(guessCount);
   }
 
+  //Start a new game
+  function newGame(){
+    guessCount = 0;
+    $("#count").text(guessCount);
+    $("#userGuess").val("");
+    $("#guessList li").remove();
+    randomNumber = parseInt(Math.floor(Math.random()*(100)), 10);
+    console.log("The new random number is " + randomNumber);
+  }
+ 
+  $(".new").click(function(){
+    newGame();
+  });
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -105,15 +119,6 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-
-  //Start a new game
-  //function newGame(){
-
-  //}
- 
-  //$(".new").click(function(){
-  //  newGame();
-  //});
 
 });
 
