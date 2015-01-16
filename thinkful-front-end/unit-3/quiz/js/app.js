397c8b91b1d0532fe62142c88876bf2681134730
Dylan Wolff <dylanwolff@gmail.com>
2015-01-17 10:52:31 +1100

    Initial JS rough out

---
+++ thinkful-front-end/unit-3/quiz/js/app.js
@@ -0,0 +1,38 @@
+//.correct, .incorrect and .totalscore all start hidden
+//User selects answer
+  //Submit on change
+  //Appropriate class in feedback shows based on whether answer is correct/incorrect
+  //User clicks arrow to move to next question
+//New question replaces value in .question (remove()/replace()?)
+//New answers replace values in .answers ul
+//When user submits response to final question (#5), .totalscore shows
+  //Next arrow becomes new game link/button
+
+$(document).ready(function() {
+
+//VARIABLES
+var currentQuestion = 0;
+var numberCorrect = 0;
+
+
+//Question & answer array
+
+
+
+//Submit answer on .answers change 
+  $('input[name=answers]').change(function() {
+    $('form').submit();
+    event.preventDefault();
+  });
+
+
+
+
+
+
+
+
+
+
+
+});
