42f962b5a95ab98a7119e53b65792d0fd8b7a03f
Dylan Wolff <dylanwolff@gmail.com>
2015-01-19 17:34:15 +1100

    Add button, tidy code

--- thinkful-front-end/unit-3/quiz/js/app.js
+++ thinkful-front-end/unit-3/quiz/js/app.js
@@ -16,50 +16,50 @@ $(document).ready(function() {
 
   //Question & answer array
   var questions = [{
-    question: "What is the longest commercial passenger route in the world (by distance)?",
-    choices: ["Sydney, Australia (SYD) to Dallas-Fort Worth, United States (DFW)",
-              "London Heathrow, United Kingdom (LHR) to Singapore (SIN)",
-              "Dubai, United Arab Emirates (DXB) to Sydney, Australia (SYD)",
-              "Tokyo Narita, Japan (NRT) to San Francisco, United States (SFO)",
-              "Beijing, China (PEK) to Chicago, United States (ORD)"],
-    qNum: 0,
-    correct: 0,
-  }, {
-    question: "Which of the following has the largest wingspan?",
-    choices: ["Airbus A340-600",
-              "Boeing 747-400ER",
-              "Boeing 777-200LR",
-              "Boeing 787-9",
-              "Boeing 767-300"],
-    qNum: 1,
-    correct: 2,
-  }, {
-    question: "During 2013, what was the world's busiest airport (by international passenger traffic)?",
-    choices: ["Hong Kong International Airport (HKG), Hong Kong",
-              "Dubai International Airport (DXB), United Arab Emirates",
-              "Singapore Changi Airport (SIN), Singapore",
-              "London Heathrow Airport (LHR), United Kingdom",
-              "Amsterdam Airport Schipol (AMS), Netherlands"],
-    qNum: 2,
-    correct: 3,
-  }, {
-    question: "What is the certified maximum passenger capacity for the Airbus A380-800 in a one-class configuration?",
-    choices: ["585",
-              "675",
-              "736",
-              "810",
-              "853"],
-    qNum: 3,
-    correct: 4,
-  }, {
-    question: "In what year did the world's first commercial flight with a paying passenger take place?",
-    choices: ["1914",
-              "1917",
-              "1925",
-              "1928",
-              "1930"],
-    qNum: 4,
-    correct: 0,
+      question: "What is the longest commercial passenger route in the world (by distance)?",
+      choices: ["Sydney, Australia (SYD) to Dallas-Fort Worth, United States (DFW)",
+                "London Heathrow, United Kingdom (LHR) to Singapore (SIN)",
+                "Dubai, United Arab Emirates (DXB) to Sydney, Australia (SYD)",
+                "Tokyo Narita, Japan (NRT) to San Francisco, United States (SFO)",
+                "Beijing, China (PEK) to Chicago, United States (ORD)"],
+      qNum: 0,
+      correct: 0
+  },  {  
+      question: "Which of the following has the largest wingspan?",
+      choices: ["Airbus A340-600",
+                "Boeing 747-400ER",
+                "Boeing 777-200LR",
+                "Boeing 787-9",
+                "Boeing 767-300"],
+      qNum: 1,
+      correct: 2
+  },  {  
+      question: "During 2013, what was the world's busiest airport (by international passenger traffic)?",
+      choices: ["Hong Kong International Airport (HKG), Hong Kong",
+                "Dubai International Airport (DXB), United Arab Emirates",
+                "Singapore Changi Airport (SIN), Singapore",
+                "London Heathrow Airport (LHR), United Kingdom",
+                "Amsterdam Airport Schipol (AMS), Netherlands"],
+      qNum: 2,
+      correct: 3
+  },  {
+      question: "What is the certified maximum passenger capacity for the Airbus A380-800 in a one-class configuration?",
+      choices: ["585",
+                "675",
+                "736",
+                "810",
+                "853"],
+      qNum: 3,
+      correct: 4
+  },  {
+      question: "In what year did the world's first commercial flight with a paying passenger take place?",
+      choices: ["1914",
+                "1917",
+                "1925",
+                "1928",
+                "1930"],
+      qNum: 4,
+      correct: 0
   }]
 
   //Add question
@@ -74,17 +74,17 @@ $(document).ready(function() {
 
   addQuestion();
 
-  //Submit answer on #answers li click
-  $("#answers li").on("click", function() {
+  //Submit answer on #button click
+  $("#button").on("click", function() {
     $('form').submit();
     console.log("answer submitted");
   });
 
   //Check user answer
-  var answer = $("input[type='radio']:checked").val();
+  var answer = $("input:radio[name='answers']:checked").val();
     if (answer == questions[currentQuestion].correct) {
-      $("#correct").show();
       numberCorrect++;
+      $("#correct").show();
       console.log("Correct answer");
     } else {
       $("#incorrect").show();
@@ -101,6 +101,8 @@ $(document).ready(function() {
       addQuestion();
     } else {
       $("#totalscore").show();
+      $("#incorrect").hide();
+      $("#correct").hide();
     }
   });
 
