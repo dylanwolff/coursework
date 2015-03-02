4ac2000d437e75e24fa53bd2913e6a48d580c0bd
Dylan Wolff <dylanwolff@gmail.com>
2015-03-02 18:03:11 +1100

    Complete exercise 3

--- pragmatic-studio/studio_game.rb
+++ pragmatic-studio/studio_game.rb
@@ -1,6 +1,9 @@
-greeting = "Welcome!"
+name1 = "larry"
+name2 = "curly"
+name3 = "moe"
 
-3.times do
-  puts greeting.upcase
-  puts Time.now
-end
+health1 = 60
+
+puts "#{name1}'s health is #{health1 / 9}"
+
+puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
