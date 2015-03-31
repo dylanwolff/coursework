067d1035271de4efcb643dbaa258aa0e2c5ce32d
Dylan Wolff <dylanwolff@gmail.com>
2015-03-31 17:28:21 +1100

    Complete exercise 8

--- pragmatic-studio/studio_game.rb
+++ pragmatic-studio/studio_game.rb
@@ -1,7 +1,14 @@
 class Player
+  attr_accessor :name
+  attr_reader :health
+
   def initialize (name, health=100)
     @name = name.capitalize
     @health = health
+  end
+
+  def score
+    @health + @name.length
   end
 
   def w00t
@@ -10,12 +17,12 @@ class Player
   end
 
   def blam
-    @health -=10
+    @health -= 10
     puts "#{@name} got blammed!"
   end
 
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 end
 
@@ -26,3 +33,4 @@ player3 = Player.new("Curly", 125)
 puts player1
 puts player2
 puts player3
+
