c9368ce83aa9f8ca026254b117a433e81c527090
Dylan Wolff <dylanwolff@gmail.com>
2015-03-31 18:55:28 +1100

    Complete exercise 10

--- pragmatic-studio/studio_game.rb
+++ pragmatic-studio/studio_game.rb
@@ -26,27 +26,38 @@ class Player
   end
 end
 
-player1 = Player.new("Moe")
-player2 = Player.new("Larry", 60)
-player3 = Player.new("Curly", 125)
+class Game
+  attr_reader :title
 
-players = [player1, player2, player3]
+  def initialize(title)
+    @title = title
+    @players = []
+  end
 
-puts "There are #{players.size} players in the game:"
-players.each do |player|
-  puts player
+  def add_player(a_player)
+    @players << a_player
+  end
+
+  def play
+    puts "There are #{@players.size} players in #{@title}:"
+    @players.each do |player|
+      puts player
+    end
+    @players.each do |player|
+      player.blam
+      player.w00t
+      player.w00t
+      puts player
+    end
+  end
 end
 
-players.each do |player|
-  puts player.health
-end
-
-players.each do |player|
-  player.blam
-  puts player
-end
-
-players.pop
-player4 = Player.new("Shemp", 90)
-players.push(player4)
-
+player1 = Player.new("moe")
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
+    
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
