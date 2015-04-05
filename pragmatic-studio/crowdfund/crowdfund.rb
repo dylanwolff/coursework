aa82561f739dfc43fcbed54b51246f15729ba720
Dylan Wolff <dylanwolff@gmail.com>
2015-04-05 20:47:28 +1000

    Complete exercise 4

--- pragmatic-studio/crowdfund/crowdfund.rb
+++ pragmatic-studio/crowdfund/crowdfund.rb
@@ -7,5 +7,11 @@ funding2 = 0
 funding3 = 0
 
 puts "#{project1} has $#{funding1} in funding."
+puts "#{project2} has $#{funding2} in funding."
+puts "#{project3} has $#{funding3} in funding."
 
 puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
+
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "Funding amounts as of #{formatted_time}."
