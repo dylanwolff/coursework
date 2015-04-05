eb9104ccff97cd15315f53927cfd0acee448003d
Dylan Wolff <dylanwolff@gmail.com>
2015-04-05 20:46:20 +1000

    Complete exercise 4

--- pragmatic-studio/studio_game_v2/studio_game.rb
+++ pragmatic-studio/studio_game_v2/studio_game.rb
@@ -1,4 +1,19 @@
-name1 = 'Larry'
-health1 = 60
+name1 = "larry"
+name2 = "curly"
+name3 = "moe"
+name4 = "shemp"
 
-puts "#{name1}'s health is #{health1 / 9}"
+health1 = 60
+health2 = 125
+health3 = 100
+health4 = 90
+
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+
+puts "The game started on #{formatted_time}"
+
+puts "#{name1.capitalize}'s health is #{health1}."
+puts "#{name2.upcase}'s health is #{health2}."
+puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
+puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
