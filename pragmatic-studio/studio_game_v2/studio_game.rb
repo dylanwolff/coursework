d86306f31fc6b6604078cf5e54db03f32c7561c9
Dylan Wolff <dylanwolff@gmail.com>
2015-04-06 06:49:52 +1000

    Complete exercise 6

--- pragmatic-studio/studio_game_v2/studio_game.rb
+++ pragmatic-studio/studio_game_v2/studio_game.rb
@@ -1,19 +1,13 @@
-name1 = "larry"
-name2 = "curly"
-name3 = "moe"
-name4 = "shemp"
+def time
+  current_time = Time.new
+  current_time.strftime("%I:%M:%S")
+end
 
-health1 = 60
-health2 = 125
-health3 = 100
-health4 = 90
+def say_hello(name, health = 100)
+  "I'm #{name.capitalize} with a health of #{health} as of #{time}."
+end
 
-current_time = Time.new
-formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
-
-puts "The game started on #{formatted_time}"
-
-puts "#{name1.capitalize}'s health is #{health1}."
-puts "#{name2.upcase}'s health is #{health2}."
-puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
-puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
+puts say_hello("larry", 60)
+puts say_hello("curly", 125)
+puts say_hello("moe", 100)
+puts say_hello("shemp", 90)
