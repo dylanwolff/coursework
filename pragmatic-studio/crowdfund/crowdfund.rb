d159022b0f1aa9285632c4eef491844443317bc2
Dylan Wolff <dylanwolff@gmail.com>
2015-04-05 20:27:37 +1000

    Setup crowdfund project

---
+++ pragmatic-studio/crowdfund/crowdfund.rb
@@ -0,0 +1,11 @@
+project1 = "Project ABC"
+project2 = "Project LMN"
+project3 = "Project XYZ"
+
+funding1 = 1000
+funding2 = 0
+funding3 = 0
+
+puts "#{project1} has $#{funding1} in funding."
+
+puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
