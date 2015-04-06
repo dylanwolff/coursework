92dac0532d5c77ec646e3059998ed3d292d08180
Dylan Wolff <dylanwolff@gmail.com>
2015-04-06 14:32:49 +1000

    Complete exercise 9

--- pragmatic-studio/studio_game_v2/studio_game.rb
+++ pragmatic-studio/studio_game_v2/studio_game.rb
@@ -27,10 +27,28 @@ class Player
 end
 
 player1 = Player.new("moe")
-puts player1
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
 
-player2 = Player.new("larry",60)
-puts player2
+players = [player1, player2, player3]
 
-player3 = Player.new("curly",125)
-puts player3
+puts "There are #{players.size} players in the game:"
+players.each do |player|
+  puts player
+end
+
+players.each do |player|
+  puts player.health
+end
+
+players.each do |player|
+  player.blam
+  player.w00t
+  player.w00t
+  puts player
+end
+
+players.pop
+player4 = Player.new("Shemp", 90)
+players.push(player4)
+puts players
