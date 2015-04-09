cf2cdcb5beaff5c57d4a87f15e5d43033d2bdb19
Dylan Wolff <dylanwolff@gmail.com>
2015-04-09 17:04:29 +1000

    Complete exercise 18

--- pragmatic-studio/studio_game_v2/player.rb
+++ pragmatic-studio/studio_game_v2/player.rb
@@ -1,3 +1,5 @@
+require_relative 'treasure_trove'
+
 class Player
   attr_accessor :name
   attr_reader :health
@@ -34,6 +36,12 @@ class Player
     @found_treasures[treasure.name] += treasure.points
     puts "#{@name} found a #{treasure.name} worth #{treasure.points} points."
     puts "#{@name}'s treasures: #{@found_treasures}"
+  end
+
+  def each_found_treasure
+    @found_treasures.each do |name, points|
+      yield Treasure.new(name, points)
+    end
   end
 
   def <=>(other)
